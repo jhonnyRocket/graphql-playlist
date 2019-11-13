0aaa54e76ecf0c6a51d4322ae7a55c1b9a7ed7c0
Jonathan <jhonny.devguru@gmail.com>
2019-11-13 07:05:46 -0300

    Connect with mongo cloud service

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+
 const schema = require('./schema/schema');
 
 const app = express();
+
+// connect to mongodb
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ping_mao:Shared123!@gql-danny-jfyx7.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log('Connected to mongodb');
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
